f4d7abfe71248c13ba23d2b6e812ee3608652758
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2023-10-14 16:25:26 +0200

    remove comment

--- src/components/dialogs/import-chip-dialog.tsx
+++ src/components/dialogs/import-chip-dialog.tsx
@@ -53,6 +53,3 @@ export const ImportChipDialog: React.FC<ImportChipDialog> = (props) => {
     </>
   );
 };
-/*
-{"name":"NAND","inputs":[{"id":"input-0","pin":"input-0_pin-0"},{"id":"input-1","pin":"input-1_pin-0"}],"outputs":[{"id":"output-0","pin":"output-0_pin-0"}],"chips":[{"id":"chip-0","coreGate":"AND","inputPins":["chip-0_input-pin-0","chip-0_input-pin-1"],"outputPins":["chip-0_output-pin-0"]},{"id":"chip-1","coreGate":"NOT","inputPins":["chip-1_input-pin-0"],"outputPins":["chip-1_output-pin-0"]}],"wires":[["input-1_pin-0","chip-0_input-pin-1"],["input-0_pin-0","chip-0_input-pin-0"],["chip-0_output-pin-0","chip-1_input-pin-0"],["chip-1_output-pin-0","output-0_pin-0"]]}
-*/
