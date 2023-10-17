ecd6d68218ac2a96a27b429f138c735b9f1f41de
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2023-10-17 08:31:43 +0200

    lint

--- src/components/toolbar/lower-toolbar.tsx
+++ src/components/toolbar/lower-toolbar.tsx
@@ -94,115 +94,115 @@ export const LowerToolbar: React.FC<LowerToolbarProps> = (props) => {
     },
   });
 
-  const blueprint1 = JSON.stringify({
-    NAND: {
-      inputs: [
-        {
-          id: "input-0",
-          pin: "chip-0_input-pin-0",
-        },
-        {
-          id: "input-1",
-          pin: "chip-0_input-pin-1",
-        },
-      ],
-      outputs: [
-        {
-          id: "output-0",
-          pin: "chip-0_output-pin-0",
-        },
-      ],
-      chips: [
-        {
-          id: "chip-0",
-          name: "AND",
-          inputPins: ["chip-0_input-pin-0", "chip-0_input-pin-1"],
-          outputPins: ["chip-0_output-pin-0"],
-        },
-        {
-          id: "chip-1",
-          name: "NOT",
-          inputPins: ["chip-1_input-pin-0"],
-          outputPins: ["chip-1_output-pin-0"],
-        },
-      ],
-      wires: [
-        ["chip-0_input-pin-1", "chip-0_input-pin-1"],
-        ["chip-0_input-pin-0", "chip-0_input-pin-0"],
-        ["chip-0_output-pin-0", "chip-1_input-pin-0"],
-        ["chip-1_output-pin-0", "chip-0_output-pin-0"],
-      ],
-    },
-    "NAND-NOT": {
-      inputs: [
-        {
-          id: "input-0",
-          pin: "chip-0_input-pin-0",
-        },
-        {
-          id: "input-1",
-          pin: "chip-0_input-pin-1",
-        },
-      ],
-      outputs: [
-        {
-          id: "output-0",
-          pin: "chip-0_output-pin-0",
-        },
-      ],
-      chips: [
-        {
-          id: "chip-0",
-          name: "NAND",
-          inputPins: ["chip-0_input-pin-0", "chip-0_input-pin-1"],
-          outputPins: ["chip-0_output-pin-0"],
-        },
-        {
-          id: "chip-1",
-          name: "NOT",
-          inputPins: ["chip-1_input-pin-0"],
-          outputPins: ["chip-1_output-pin-0"],
-        },
-      ],
-      wires: [
-        ["chip-0_input-pin-1", "chip-0_input-pin-1"],
-        ["chip-0_input-pin-0", "chip-0_input-pin-0"],
-        ["chip-0_output-pin-0", "chip-1_input-pin-0"],
-        ["chip-1_output-pin-0", "chip-0_output-pin-0"],
-      ],
-    },
-    main: {
-      inputs: [
-        {
-          id: "input-0",
-          pin: "input-0_pin-0",
-        },
-        {
-          id: "input-1",
-          pin: "input-1_pin-0",
-        },
-      ],
-      outputs: [
-        {
-          id: "output-0",
-          pin: "output-0_pin-0",
-        },
-      ],
-      chips: [
-        {
-          id: "chip-0",
-          name: "NAND-NOT",
-          inputPins: ["chip-0_input-pin-0", "chip-0_input-pin-1"],
-          outputPins: ["chip-0_output-pin-0"],
-        },
-      ],
-      wires: [
-        ["input-1_pin-0", "chip-0_input-pin-1"],
-        ["input-0_pin-0", "chip-0_input-pin-0"],
-        ["chip-0_output-pin-0", "output-0_pin-0"],
-      ],
-    },
-  });
+  // const blueprint1 = JSON.stringify({
+  //   NAND: {
+  //     inputs: [
+  //       {
+  //         id: "input-0",
+  //         pin: "chip-0_input-pin-0",
+  //       },
+  //       {
+  //         id: "input-1",
+  //         pin: "chip-0_input-pin-1",
+  //       },
+  //     ],
+  //     outputs: [
+  //       {
+  //         id: "output-0",
+  //         pin: "chip-0_output-pin-0",
+  //       },
+  //     ],
+  //     chips: [
+  //       {
+  //         id: "chip-0",
+  //         name: "AND",
+  //         inputPins: ["chip-0_input-pin-0", "chip-0_input-pin-1"],
+  //         outputPins: ["chip-0_output-pin-0"],
+  //       },
+  //       {
+  //         id: "chip-1",
+  //         name: "NOT",
+  //         inputPins: ["chip-1_input-pin-0"],
+  //         outputPins: ["chip-1_output-pin-0"],
+  //       },
+  //     ],
+  //     wires: [
+  //       ["chip-0_input-pin-1", "chip-0_input-pin-1"],
+  //       ["chip-0_input-pin-0", "chip-0_input-pin-0"],
+  //       ["chip-0_output-pin-0", "chip-1_input-pin-0"],
+  //       ["chip-1_output-pin-0", "chip-0_output-pin-0"],
+  //     ],
+  //   },
+  //   "NAND-NOT": {
+  //     inputs: [
+  //       {
+  //         id: "input-0",
+  //         pin: "chip-0_input-pin-0",
+  //       },
+  //       {
+  //         id: "input-1",
+  //         pin: "chip-0_input-pin-1",
+  //       },
+  //     ],
+  //     outputs: [
+  //       {
+  //         id: "output-0",
+  //         pin: "chip-0_output-pin-0",
+  //       },
+  //     ],
+  //     chips: [
+  //       {
+  //         id: "chip-0",
+  //         name: "NAND",
+  //         inputPins: ["chip-0_input-pin-0", "chip-0_input-pin-1"],
+  //         outputPins: ["chip-0_output-pin-0"],
+  //       },
+  //       {
+  //         id: "chip-1",
+  //         name: "NOT",
+  //         inputPins: ["chip-1_input-pin-0"],
+  //         outputPins: ["chip-1_output-pin-0"],
+  //       },
+  //     ],
+  //     wires: [
+  //       ["chip-0_input-pin-1", "chip-0_input-pin-1"],
+  //       ["chip-0_input-pin-0", "chip-0_input-pin-0"],
+  //       ["chip-0_output-pin-0", "chip-1_input-pin-0"],
+  //       ["chip-1_output-pin-0", "chip-0_output-pin-0"],
+  //     ],
+  //   },
+  //   main: {
+  //     inputs: [
+  //       {
+  //         id: "input-0",
+  //         pin: "input-0_pin-0",
+  //       },
+  //       {
+  //         id: "input-1",
+  //         pin: "input-1_pin-0",
+  //       },
+  //     ],
+  //     outputs: [
+  //       {
+  //         id: "output-0",
+  //         pin: "output-0_pin-0",
+  //       },
+  //     ],
+  //     chips: [
+  //       {
+  //         id: "chip-0",
+  //         name: "NAND-NOT",
+  //         inputPins: ["chip-0_input-pin-0", "chip-0_input-pin-1"],
+  //         outputPins: ["chip-0_output-pin-0"],
+  //       },
+  //     ],
+  //     wires: [
+  //       ["input-1_pin-0", "chip-0_input-pin-1"],
+  //       ["input-0_pin-0", "chip-0_input-pin-0"],
+  //       ["chip-0_output-pin-0", "output-0_pin-0"],
+  //     ],
+  //   },
+  // });
 
   return (
     <div className={`${styles.toolbar} ${styles.lowerToolbar}`}>
