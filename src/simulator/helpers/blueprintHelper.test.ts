b2b62a5721b89b869b7d05df847f6e13506bd22c
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2023-10-22 11:05:34 +0200

    working test

--- src/simulator/helpers/blueprintHelper.test.ts
+++ src/simulator/helpers/blueprintHelper.test.ts
@@ -147,42 +147,42 @@ const SCHEMA_DATA_LATCH = {
     ],
     outputs: [
       {
-        id: "chip.output.17",
+        id: "chip.output.2",
       },
     ],
     chips: [
       {
-        id: "chip.AND.2",
+        id: "chip.AND.3", //changed
         name: "AND",
       },
       {
-        id: "chip.AND.3",
+        id: "chip.AND.4",
         name: "AND",
       },
       {
-        id: "chip.NOT.4",
+        id: "chip.NOT.5",
         name: "NOT",
       },
       {
-        id: "chip.NOR.10",
+        id: "chip.NOR.11",
         name: "NOR",
       },
       {
-        id: "chip.NOR.16",
+        id: "chip.NOR.17",
         name: "NOR",
       },
     ],
     wires: [
-      ["chip.NOR.16.output.0/output.0", "chip.output.17/input.0"],
-      ["chip.NOR.10.output.0/output.0", "chip.NOR.16.input.0/input.0"],
-      ["chip.NOR.16.output.0/output.0", "chip.NOR.10.input.1/input.0"],
-      ["chip.AND.2/output.0", "chip.NOR.10.input.0/input.0"],
-      ["chip.AND.3/output.0", "chip.NOR.16.input.1/input.0"],
-      ["chip.NOT.4/output.0", "chip.AND.3/input.1"],
-      ["chip.input.1/output.0", "chip.AND.3/input.0"],
-      ["chip.input.1/output.0", "chip.AND.2/input.1"],
-      ["chip.input.0/output.0", "chip.NOT.4/input.0"],
-      ["chip.input.0/output.0", "chip.AND.2/input.0"],
+      ["chip.NOR.17.output.0/output.0", "chip.output.2/input.0"],
+      ["chip.NOR.11.output.0/output.0", "chip.NOR.17.input.0/input.0"],
+      ["chip.NOR.17.output.0/output.0", "chip.NOR.11.input.1/input.0"],
+      ["chip.AND.3/output.0", "chip.NOR.11.input.0/input.0"], // start
+      ["chip.AND.4/output.0", "chip.NOR.17.input.1/input.0"],
+      ["chip.NOT.5/output.0", "chip.AND.4/input.1"],
+      ["chip.input.1/output.0", "chip.AND.4/input.0"],
+      ["chip.input.1/output.0", "chip.AND.3/input.1"], // end
+      ["chip.input.0/output.0", "chip.NOT.5/input.0"],
+      ["chip.input.0/output.0", "chip.AND.3/input.0"], //end
     ],
   },
 };
@@ -380,7 +380,7 @@ describe("BlueprintHelper", () => {
       }
     });
 
-    test.skip("returns the blueprint for a Data Latch", () => {
+    test("returns the blueprint for a Data Latch", () => {
       const dataLatchBlueprint = `{"NOR":{"inputs":[{"id":"chip.NOR.16.input.0"},{"id":"chip.NOR.16.input.1"}],"outputs":[{"id":"chip.NOR.16.output.0"}],"chips":[{"id":"chip.OR.14","name":"OR"},{"id":"chip.NOT.15","name":"NOT"}],"wires":[["chip.OR.14/output.0","chip.NOT.15/input.0"],["chip.NOR.16.input.0/input.0","chip.OR.14/input.0"],["chip.NOR.16.input.1/input.0","chip.OR.14/input.1"],["chip.NOT.15/output.0","chip.NOR.16.output.0/output.0"]]},"main":{"inputs":[{"id":"chip.input.0"},{"id":"chip.input.1"}],"outputs":[{"id":"chip.output.17"}],"chips":[{"id":"chip.AND.2","name":"AND"},{"id":"chip.AND.3","name":"AND"},{"id":"chip.NOT.4","name":"NOT"},{"id":"chip.NOR.10","name":"NOR"},{"id":"chip.NOR.16","name":"NOR"}],"wires":[["chip.NOR.16.output.0/output.0","chip.output.17/input.0"],["chip.NOR.10.output.0/output.0","chip.NOR.16.input.0/input.0"],["chip.NOR.16.output.0/output.0","chip.NOR.10.input.1/input.0"],["chip.AND.2/output.0","chip.NOR.10.input.0/input.0"],["chip.AND.3/output.0","chip.NOR.16.input.1/input.0"],["chip.NOT.4/output.0","chip.AND.3/input.1"],["chip.input.1/output.0","chip.AND.3/input.0"],["chip.input.1/output.0","chip.AND.2/input.1"],["chip.input.0/output.0","chip.NOT.4/input.0"],["chip.input.0/output.0","chip.AND.2/input.0"]]}}`;
       const dataLatchCircuit = BlueprintHelper.blueprintToCircuit(
         p,
