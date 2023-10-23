dc13d7ee8f2496dcf54ae6ea74327d4e08c4bab4
Hasnain Roopawalla <37022937+hasnainroopawalla@users.noreply.github.com>
2023-10-23 23:33:51 +0200

    tests (#18)

---
+++ src/color-generator/color-generator.test.ts
@@ -0,0 +1,15 @@
+import { colorGenerator } from "./color-generator";
+import { colors } from "./colors";
+
+const generatedColors: string[] = [];
+
+describe("ColorGenerator", () => {
+  test.each(Array.from(Array(colors.length).keys()))(
+    "generated color is not repeated",
+    () => {
+      const generatedColor = colorGenerator.generate();
+      expect(generatedColors).not.toContain(generatedColor);
+      generatedColors.push(generatedColor);
+    }
+  );
+});
---
+++ src/components/factory/button.test.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { Button } from "./button";
+
+const BUTTON_TID = "buttonTestId";
+const onClick = jest.fn();
+
+describe("Button", () => {
+  beforeEach(() => {
+    render(
+      <Button
+        text="buttonText"
+        dataTestId={BUTTON_TID}
+        onClick={onClick}
+        size="small"
+      />
+    );
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test.only("button with text is visible", async () => {
+    const button = screen.getByTestId(BUTTON_TID);
+    expect(button.textContent).toBe("buttonText");
+    expect(button).toBeVisible();
+  });
+});
