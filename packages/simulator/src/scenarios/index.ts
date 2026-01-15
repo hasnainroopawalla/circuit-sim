2c95d46fffc2a3a7b8241628de62fbf5287563b2
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2026-01-15 15:06:35 +0530

    scenario loader

---
+++ packages/simulator/src/scenarios/index.ts
@@ -0,0 +1,1 @@
+export { ScenarioLoader } from "./scenario-loader";
--- packages/simulator/src/simulator-app.ts
+++ packages/simulator/src/simulator-app.ts
@@ -3,6 +3,7 @@ import { Simulator } from "./simulator";
 import { Clock } from "./clock";
 import { Camera } from "./camera";
 import { MeshUtils } from "./mesh-utils";
+import { ScenarioLoader } from "./scenarios";
 
 // managers
 import { LayoutManager } from "./layouts/layout-manager";
@@ -31,6 +32,8 @@ export class SimulatorApp {
 	private camera: Camera;
 
 	private animationId: number | null = null;
+
+	private scenarioLoader: ScenarioLoader;
 
 	constructor(args: SimulatorAppArgs) {
 		this.clock = new Clock({ showFrameTime: false });
@@ -69,13 +72,14 @@ export class SimulatorApp {
 			this.registerCanvasResizeObserver(args.canvas);
 		});
 
+		this.scenarioLoader = new ScenarioLoader(this.sim);
+
 		this.registerSubscriptions();
 	}
 
 	public async start(): Promise<void> {
 		if (!this.animationId) {
 			await this.renderEngineInitPromise;
-			this.setupNandGate(); // TODO: remove
 			this.loop();
 		}
 	}
@@ -167,200 +171,5 @@ export class SimulatorApp {
 		this.sim.chipManager.reset();
 		this.sim.wireManager.reset();
 		this.overlayManager.reset();
-	}
-
-	public setupNandGate(): void {
-		const spawnNand = () => {
-			const andChipSpec = this.sim.chipLibraryService.getChipFactory({
-				kind: "atomic",
-				name: "AND",
-			});
-
-			const notChipSpec = this.sim.chipLibraryService.getChipFactory({
-				kind: "atomic",
-				name: "NOT",
-			});
-
-			const inputChipSpec = this.sim.chipLibraryService.getChipFactory({
-				kind: "io",
-				name: "input",
-			});
-
-			const outputChipSpec = this.sim.chipLibraryService.getChipFactory({
-				kind: "io",
-				name: "output",
-			});
-
-			// INPUT 0
-			const inputChip0 = this.sim.chipManager.spawnChip(inputChipSpec, {
-				color: { r: 0, g: 0, b: 0, a: 1 },
-				position: { x: 1.6, y: 1.3 },
-			});
-			// INPUT 1
-			const inputChip1 = this.sim.chipManager.spawnChip(inputChipSpec, {
-				color: { r: 0, g: 0, b: 0, a: 1 },
-				position: { x: 1.6, y: 0.8 },
-			});
-			// AND
-			const andChip = this.sim.chipManager.spawnChip(andChipSpec, {
-				color: { r: 0, g: 0, b: 0, a: 1 },
-				position: { x: 0.3, y: 1 },
-			});
-			// NOT
-			const notChip = this.sim.chipManager.spawnChip(notChipSpec, {
-				color: { r: 0, g: 0, b: 0, a: 1 },
-				position: { x: -1, y: 1 },
-			});
-			// OUTPUT 0
-			const outputChip = this.sim.chipManager.spawnChip(outputChipSpec, {
-				color: { r: 0, g: 0, b: 0, a: 1 },
-				position: { x: -2, y: 1 },
-			});
-			// input 0 to AND in 0
-			this.sim.wireManager.spawnWire(
-				{
-					startPin: inputChip0.getPin(),
-					endPin: andChip.getPin("in0")!,
-				},
-				{
-					color: { r: 0, g: 0, b: 0, a: 1 },
-					controlPoints: [],
-				},
-			);
-			// input 1 to AND in 1
-			this.sim.wireManager.spawnWire(
-				{
-					startPin: inputChip1.getPin(),
-					endPin: andChip.getPin("in1")!,
-				},
-				{
-					color: { r: 0, g: 0, b: 0, a: 1 },
-					controlPoints: [],
-				},
-			);
-			// AND out to NOT in
-			this.sim.wireManager.spawnWire(
-				{
-					startPin: andChip.getPin("out0")!,
-					endPin: notChip.getPin("in0")!,
-				},
-				{
-					color: { r: 0, g: 0, b: 0, a: 1 },
-					controlPoints: [],
-				},
-			);
-			// NOT out to output
-			this.sim.wireManager.spawnWire(
-				{
-					startPin: notChip.getPin("out0")!,
-					endPin: outputChip.getPin(),
-				},
-				{
-					color: { r: 0, g: 0, b: 0, a: 1 },
-					controlPoints: [],
-				},
-			);
-		};
-
-		const spawnCompositeNand = () => {
-			const compositeAndChipFactory =
-				this.sim.chipLibraryService.getChipFactory({
-					kind: "composite",
-					name: "composite-AND",
-				});
-
-			const notChipFactory = this.sim.chipLibraryService.getChipFactory({
-				kind: "atomic",
-				name: "NOT",
-			});
-
-			const inputChipFactory = this.sim.chipLibraryService.getChipFactory({
-				kind: "io",
-				name: "input",
-			});
-
-			const outputChipFactory = this.sim.chipLibraryService.getChipFactory({
-				kind: "io",
-				name: "output",
-			});
-
-			// INPUT 0
-			const inputChip0 = this.sim.chipManager.spawnChip(inputChipFactory, {
-				color: { r: 0, g: 0, b: 0, a: 1 },
-				position: { x: 1.6, y: 1.3 },
-			});
-			// INPUT 1
-			const inputChip1 = this.sim.chipManager.spawnChip(inputChipFactory, {
-				color: { r: 0, g: 0, b: 0, a: 1 },
-				position: { x: 1.6, y: 0.8 },
-			});
-			// AND
-			const compositeAndChip = this.sim.chipManager.spawnChip(
-				compositeAndChipFactory,
-				{
-					color: { r: 0, g: 0, b: 0, a: 1 },
-					position: { x: 0.3, y: 1 },
-				},
-			);
-			// NOT
-			const notChip = this.sim.chipManager.spawnChip(notChipFactory, {
-				color: { r: 0, g: 0, b: 0, a: 1 },
-				position: { x: -1, y: 1 },
-			});
-			// OUTPUT 0
-			const outputChip = this.sim.chipManager.spawnChip(outputChipFactory, {
-				color: { r: 0, g: 0, b: 0, a: 1 },
-				position: { x: -2, y: 1 },
-			});
-			// input 0 to AND in 0
-			this.sim.wireManager.spawnWire(
-				{
-					startPin: inputChip0.getPin(),
-					endPin: compositeAndChip.getPin("C-AND A")!,
-				},
-				{
-					color: { r: 0, g: 0, b: 0, a: 1 },
-					controlPoints: [],
-				},
-			);
-			// input 1 to AND in 1
-			this.sim.wireManager.spawnWire(
-				{
-					startPin: inputChip1.getPin(),
-					endPin: compositeAndChip.getPin("C-AND B")!,
-				},
-				{
-					color: { r: 0, g: 0, b: 0, a: 1 },
-					controlPoints: [],
-				},
-			);
-			// AND out to NOT in
-			this.sim.wireManager.spawnWire(
-				{
-					startPin: compositeAndChip.getPin("C-AND OUT")!,
-					endPin: notChip.getPin("in0")!,
-				},
-				{
-					color: { r: 0, g: 0, b: 0, a: 1 },
-					controlPoints: [],
-				},
-			);
-			// NOT out to output
-			this.sim.wireManager.spawnWire(
-				{
-					startPin: notChip.getPin("out0")!,
-					endPin: outputChip.getPin(),
-				},
-				{
-					color: { r: 0, g: 0, b: 0, a: 1 },
-					controlPoints: [],
-				},
-			);
-		};
-
-		// this.sim.blueprintService.loadBlueprint(srLatch);
-
-		// spawnNand();
-		// spawnCompositeNand();
 	}
 }
