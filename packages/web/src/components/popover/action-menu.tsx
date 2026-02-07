8d75f5d2dfcb55e20bc4c4f7eb5d7f619d329bc1
Hasnain Roopawalla <hasnain.roopawalla@gmail.com>
2026-02-07 14:54:57 +0530

    working

--- packages/web/src/components/popover/action-menu.tsx
+++ packages/web/src/components/popover/action-menu.tsx
@@ -1,30 +1,27 @@
 import type * as React from "react";
 
+export type ActionMenuItem = {
+  label: string;
+  handler: () => void;
+};
+
 type ActionMenuProps = {
-	onView: () => void;
-	onDelete: () => void;
+  items: ActionMenuItem[];
 };
 
-export const ActionMenu: React.FC<ActionMenuProps> = ({ onView, onDelete }) => {
-	return (
-		<div className="py-1 text-sm text-white">
-			<button
-				type="button"
-				onClick={onView}
-				className="flex items-center w-full gap-2 px-3 py-2 cursor-pointer hover:bg-white/5"
-			>
-				View
-			</button>
-
-			<button
-				type="button"
-				onClick={onDelete}
-				className="flex items-center w-full gap-2 px-3 py-2 cursor-pointer text-rose-400 hover:bg-white/5"
-			>
-				Delete
-			</button>
-		</div>
-	);
+export const ActionMenu: React.FC<ActionMenuProps> = ({ items }) => {
+  return (
+    <div className="py-1 text-sm text-white">
+      {items.map((item) => (
+        <button
+          type="button"
+          key={item.label}
+          onClick={item.handler}
+          className="flex items-center w-full gap-2 px-3 py-2 cursor-pointer hover:bg-white/5"
+        >
+          {item.label}
+        </button>
+      ))}
+    </div>
+  );
 };
-
-export default ActionMenu;
--- packages/web/src/components/popover/popover.tsx
+++ packages/web/src/components/popover/popover.tsx
@@ -1,60 +1,101 @@
 import * as React from "react";
 import { useOnClickOutside } from "../../utils";
 import { useSimulatorApp } from "../../contexts/simulator-app-context";
-import type { IEntitySecondaryActionEvent } from "@digital-logic-sim/simulator";
-import { ActionMenu } from "./action-menu";
+import type {
+  IEntitySecondaryActionEvent,
+  SimulatorApp,
+} from "@digital-logic-sim/simulator";
+import { ActionMenu, type ActionMenuItem } from "./action-menu";
 
 export const Popover: React.FC = () => {
-	const simulatorApp = useSimulatorApp();
+  const simulatorApp = useSimulatorApp();
 
-	const divRef = React.useRef<HTMLDivElement>(null);
+  const divRef = React.useRef<HTMLDivElement>(null);
 
-	const [popoverData, setPopoverData] =
-		React.useState<IEntitySecondaryActionEvent | null>(null);
+  const [popoverData, setPopoverData] =
+    React.useState<IEntitySecondaryActionEvent | null>(null);
 
-	const closePopover = React.useCallback(() => {
-		setPopoverData(null);
-	}, []);
+  const closePopover = React.useCallback(() => {
+    setPopoverData(null);
+  }, []);
 
-	useOnClickOutside(divRef, closePopover);
+  useOnClickOutside(divRef, closePopover);
 
-	React.useEffect(() => {
-		const unubscribe = simulatorApp.sim.on("entity.secondaryAction", (data) => {
-			setPopoverData(data);
-		});
+  React.useEffect(() => {
+    const unubscribe = simulatorApp.sim.on("entity.secondaryAction", (data) => {
+      setPopoverData(data);
+    });
 
-		return () => {
-			unubscribe();
-		};
-	}, [simulatorApp]);
+    return () => {
+      unubscribe();
+    };
+  }, [simulatorApp]);
 
-	return (
-		popoverData && (
-			<div
-				ref={divRef}
-				className="fixed z-50 rounded-lg shadow-lg bg-neutral-900/90 backdrop-blur-xl ring-1 ring-white/10"
-				style={{
-					top: `${popoverData.mousePosition.y}px`,
-					left: `${popoverData.mousePosition.x}px`,
-				}}
-			>
-				<div className="overflow-hidden">
-					<ActionMenu
-						onView={() => {
-							console.log("view", popoverData);
-							setPopoverData(null);
-						}}
-						onDelete={() => {
-							console.log("delete", popoverData);
-							setPopoverData(null);
-						}}
-					/>
-				</div>
-			</div>
-		)
-	);
+  const items = React.useMemo(() => {
+    if (!popoverData) {
+      return [];
+    }
+
+    return getActionsForEntity(popoverData, simulatorApp).map((action) => ({
+      ...action,
+      handler: () => {
+        action.handler();
+        setPopoverData(null);
+      },
+    }));
+  }, [popoverData, simulatorApp]);
+
+  return (
+    popoverData && (
+      <div
+        ref={divRef}
+        className="fixed z-50 rounded-lg shadow-lg bg-neutral-900/90 backdrop-blur-xl ring-1 ring-white/10"
+        style={{
+          top: `${popoverData.mousePosition.y}px`,
+          left: `${popoverData.mousePosition.x}px`,
+        }}
+      >
+        <div className="overflow-hidden">
+          <ActionMenu items={items} />
+        </div>
+      </div>
+    )
+  );
 };
 
-function renderActionComponent(action: unknown): React.ReactNode {
-	return <div>{JSON.stringify(action)}</div>;
+function getActionsForEntity(
+  data: IEntitySecondaryActionEvent,
+  simulatorApp: SimulatorApp,
+): ActionMenuItem[] {
+  switch (data.entityType) {
+    case "chip":
+      return [
+        {
+          label: "View",
+          handler: () => {
+            simulatorApp.sim.emit("view.composite-chip", {
+              compositeChipId: data.entityType,
+            });
+          },
+        },
+        {
+          label: "Delete",
+          handler: () => {},
+        },
+      ];
+
+    case "wire":
+      return [
+        {
+          label: "Delete",
+          handler: () => {},
+        },
+      ];
+
+    case "pin":
+      return [];
+
+    default:
+      return [];
+  }
 }
